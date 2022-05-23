51f1b6704bf2ab24516c0420133328a9391b5e98
LisaW <LisaW@ya.ru>
2022-05-23 23:34:51 +0300

    fix: add exit behavior to 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,9 +4,15 @@ const { stdin, stdout } = process;
 const writableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf8', {
   flags: 'a+'
 });
-stdout.write('Please, tipe some text\n');
+stdout.write('Please, type some text\n');
 
 stdin.on('data', (data)=>{
-  if (data.toString().trim() === 'exit')  process.exit();
+  if (data.toString().trim() === 'exit') process.exit();
   writableStream.write(data.toString());
+});
+process.on('exit', ()=>{
+  stdout.write('Bye!\n');
+});
+process.on('SIGINT', () => {
+  process.exit();
 });
